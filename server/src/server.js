7b11511d210d4bb171d34e1e9ff78e58adc894b1
lakshayyadav10 <lakshayyadav2463@gmail.com>
2026-05-19 03:20:49 +0530

    Prepare full-stack AI job tracker for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,23 @@
-require('dotenv').config()
+const dns = require("dns");
 
-const app = require('./app')
-const connectDB = require('./config/db')
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-const PORT = process.env.PORT || 5000
 
-async function startServer() {
-  await connectDB()
+
+
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  await connectDB();
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
-}
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
 
-startServer()
+startServer();
